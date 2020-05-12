0fbe20827a81a686359d740e060848837cc7f956
Danijela <56878144+Danijela321@users.noreply.github.com>
2020-05-12 17:26:21 +0200

    JPA

---
+++ src/Elever.java
@@ -0,0 +1,35 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * 
+ * @author Danijela
+ *
+ */
+
+@Entity
+public class Elever implements java.io.Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String firstName;
+	private String surname;
+	private String skola;
+	private int klass;
+	
+	public Elever() {}
+	
+	public Elever(String firstName, String surname, String skola, int klass) {
+		super();
+		this.firstName = firstName;
+		this.surname = surname;
+		this.skola = skola;
+		this.klass = klass;
+	}
+	
+	public String toString() {
+		return "Elev: " + this.firstName + " " + this.surname+ " "+this.skola+ " "+this.klass;
+	}
+}
---
+++ src/TestJPA.java
@@ -0,0 +1,39 @@
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ * 
+ * @author Danijela
+ *
+ */
+public class TestJPA {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("eleverDb");
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		//Elever elever1 = new Elever("Marcus", "Andersson", "Teknikhogskolan", 2);
+		Elever elever1 = new Elever("Bjorn", "Andersson", "Folkhogskolan", 1);
+		em.persist(elever1);
+
+		Query q = em.createQuery("select elever from Elever elever");
+
+		List<Elever> results = q.getResultList();
+
+		for (Elever eleverna : results) {
+			System.out.println(eleverna);
+		}
+		tx.commit();
+		em.close();
+
+	}
+
+}
